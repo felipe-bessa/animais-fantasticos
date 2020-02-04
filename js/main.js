b592b7cba35309bb6637601c8c1c8d2e1d423d1b
Felipe Bessa <felipe-bessa@outlook.com>
2020-02-04 17:53:22 -0300

    Adicionando a navegação dos paineis dos animais

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ console.log(primeiraUl);
 
 const allInternalLinks = document.querySelectorAll('[href^="#"]');
 console.log(allInternalLinks);
-*/
+************************************************************************
 
 //Retorne no conosole todas as imagens do site
 const images = document.querySelectorAll('img');
@@ -44,3 +44,43 @@ console.log(primeiroH2AnimaisDescricao);
 //Selecione o ultimo p do site
 const ultimoP = Array.from(document.querySelectorAll('p')).pop();
 console.log(ultimoP);
+
+//Adicione a classe ativo a todos os itens do menu
+const menuItems = document.querySelectorAll('.menu ul li');
+menuItems.forEach((item) => item.classList.add('ativo'));
+
+//Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+menuItems.forEach((item, index) => {
+  if(index >= 1) {
+    item.classList.remove('ativo');
+  }
+});
+
+//Verifique se as imagens tem o atributo "alt"
+//OBS as Imagens já foram selecionadas previamente em exercicio anterior
+images.forEach((image) => {
+  console.log(image.hasAttribute('alt'));
+});
+
+//Modifique o "href" do link externo do menu
+const linkExterno = document.querySelector('a[href^="http"]');
+linkExterno.setAttribute('href', 'https://www.google.com');
+*/
+
+//Selecionando as imagens dos animais e as areas de conteúdo para fazer a páginação dinâmica
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+//Função para habilitar uma descrição de um animal em função de um número.
+function activeTab(index) {
+  tabContent.forEach((section) => {
+    section.classList.remove('ativo');
+  });
+  tabContent[index].classList.add('ativo');
+}
+
+tabMenu.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    activeTab(index);
+  });
+});
